bfa6790de8939c4ca84ab424707035c468751e49
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2016-02-15 20:08:34 +0400

    Update versions.php
    build 7 support

--- src/PocketPE/versions.php
+++ src/PocketPE/versions.php
@@ -9,7 +9,7 @@ class Versions extends PocketPE {
  
  public function Versions() {
 Packets>>playerVersion  
-get.clientVersion=0.14.0b2 get.protocolVersion=40
+get.clientVersion=0.14.0b7 get.protocolVersion=40
  correctVersion.connectYes
  incorrectVersion.connectNo 
   if=connectNo>>pingmsg="Invalid client"
